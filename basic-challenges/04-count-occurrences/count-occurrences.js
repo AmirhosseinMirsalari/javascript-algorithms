fb7bad003c4cacea5c361d76bf11cfb651545fe3
AmirhosseinMirsalari <amir.tk7@gmail.com>
2024-02-18 18:19:16 +0330

    Count Occurrences

---
+++ basic-challenges/04-count-occurrences/count-occurrences.js
@@ -0,0 +1,11 @@
+function countOccurrences(string, character) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === character) {
+      count++;
+    }
+  }
+  return count;
+}
+
+module.exports = countOccurrences;
